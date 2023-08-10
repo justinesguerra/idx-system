c1976c2849c244bd04518909f9796a6ac0505176
Justin Esguerra <esguerra.justin28@gmail.com>
2023-08-10 18:22:20 +0800

    new api

--- database/seeders/AppConfigSeeder.php
+++ database/seeders/AppConfigSeeder.php
@@ -20,9 +20,9 @@ class AppConfigSeeder extends Seeder
         $data = [
             ["label" => "API_USERNAME", "value" => "admin_uw5rki1o"],
             ["label" => "API_PASSWORD", "value" => "5KkoIO~uTI%1Coc4"],
-            ["label" => "LISTINGS_URL", "value" => "https://weilcoravestijn.realtyna.info/wp-json/mo/v1/Listings"],
-            ["label" => "LEADS_URL", "value" => "https://weilcoravestijn.realtyna.info/wp-json/mo/v1/Leads"],
-            ["label" => "FAVORITES_URL", "value" => "https://weilcoravestijn.realtyna.info/wp-json/mo/v1/pro-favorites/"],
+            ["label" => "LISTINGS_URL", "value" => "https://homesandlifestyle.com/wp-json/mo/v1/Listings"],
+            ["label" => "LEADS_URL", "value" => "https://homesandlifestyle.com/wp-json/mo/v1/Leads"],
+            ["label" => "FAVORITES_URL", "value" => "https://homesandlifestyle.com/wp-json/mo/v1/pro-favorites/"],
         ];
 
         AppConfig::insert($data);
